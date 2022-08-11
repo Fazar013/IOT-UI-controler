9bd02b5d94d698e70c8796ba3a89c1d73bbf9933
Fazar013 <76737163+Fazar013@users.noreply.github.com>
2022-08-11 10:54:16 +0700

    update
    some

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -105,7 +105,7 @@ class _HomePageState extends State<HomePage> {
                                         fontWeight: FontWeight.normal),
                                   ),
                                   Text(
-                                    "Living Room",
+                                    "Equipment room",
                                     style: TextStyle(
                                         height: 1.1,
                                         fontSize: 17,
